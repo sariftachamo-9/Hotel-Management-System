c1b9d2a0253db61a18ac2b9151ffc37b71be478a
Sarif Tachamo <112578172+sariftachamo-9@users.noreply.github.com>
2026-04-01 13:42:16 +0545

    Delete learning directory

--- learning/Hotel MS/routes/common.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-common_bp = Blueprint('common', __name__, url_prefix='/api')
-
-from routes import common_routes
--- learning/Hotel MS/routes/receptionist.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-receptionist_bp = Blueprint('receptionist', __name__, url_prefix='/api/receptionist')
-
-from routes import receptionist_routes
